19f13dbacdad2ce66cbd8b9b46e44cd76f0c5807
Josh Ghent <josh.ghent@capp.co>
2019-10-08 11:29:03 +0100

    Improved now page contents

--- src/pages/now.js
+++ src/pages/now.js
@@ -19,23 +19,35 @@ class HomeIndex extends React.Component {
 					<h1>/now</h1>
 					<p>Inspired by the <a href="https://nownownow.com/about">/now page movement</a>. The purpose of this page is to share what I'm currently focused on.</p>
 
+					<h2>What are my interests?</h2>
+					<ul>
+						<li>Cloud Architecture</li>
+						<li>Infrastructure-as-code (Terraform)</li>
+						<li>Self-Hosted Software</li>
+						<li>Security Engineering</li>
+						<li>Resiliency</li>
+						<li>Automation</li>
+					</ul>
+
 					<h2>What am I up to?</h2>
 					<ul>
-						<li>Buying my first house</li>
-						<li>Getting married!</li>
+						<li>Doing lots of DIY in my first home with my new wife</li>
+						<li>I work as a Senior Software Engineer at [Capp & Co.](https://capp.co)</li>
+						<li>Kitting out my home office to enable me to transition to more remote work</li>
 						<li>Organizing <a href="https://meetup.com/leicesterjs">LeicesterJS</a></li>
-						<li>Learning about Cloud Architecture and Software Design Patterns</li>
+						<li>Helping re-architect the third-party integrations system to be more modern</li>
+						<li>Enhancing my knowledge about Cloud Architecture and Software Design Patterns</li>
 					</ul>
 
 					<h2>What tech am I using</h2>
 					<ul>
-						<li>I work as a Senior Software Engineer at Capp & Co.</li>
-						<li>At work and home I use a Macbook Pro of varying years (2014 at home, 2017 at work)</li>
+						<li>At work and home I use a Macbook Pro of varying years (2014 at home, 2019 at work)</li>
 						<li>Primarily using Typescript and .NET Core to write new services</li>
 						<li>Using AWS to deploy and host</li>
-						<li>Configuring lots of bots/automations via Docker, Zapier, IFTTT and a personal Slack workspace</li>
+						<li>Configuring lots of bots/automations via Docker, Zapier, IFTTT, Shortcuts and a personal Slack workspace</li>
 						<li>I use an iPhone 8+ as my mobile device for security and usability reasons - don't @ me</li>
 						<li>I have a gaming desktop, which I seldom use and is mostly used for cord cutting services (sonarr, radarr etc)</li>
+						<li>You can find the setup for my machines (which are standardized) [here](https://github.com/joshghent/dotfiles)</li>
 					</ul>
 				</div>
 			</Layout>
