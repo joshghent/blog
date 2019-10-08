fdc3dcc51efe08b82eb9d17f0f3560c76edbfaa3
Josh Ghent <josh.ghent@capp.co>
2019-10-08 16:41:27 +0100

    Fixed now page formatting - forgot it's JSX not markdown

--- src/pages/now.js
+++ src/pages/now.js
@@ -32,7 +32,7 @@ class HomeIndex extends React.Component {
 					<h2>What am I up to?</h2>
 					<ul>
 						<li>Doing lots of DIY in my first home with my new wife</li>
-						<li>I work as a Senior Software Engineer at [Capp & Co.](https://capp.co)</li>
+						<li>I work as a Senior Software Engineer at <a href="https://capp.co">Capp & Co.</a></li>
 						<li>Kitting out my home office to enable me to transition to more remote work</li>
 						<li>Organizing <a href="https://meetup.com/leicesterjs">LeicesterJS</a></li>
 						<li>Helping re-architect the third-party integrations system to be more modern</li>
@@ -47,7 +47,7 @@ class HomeIndex extends React.Component {
 						<li>Configuring lots of bots/automations via Docker, Zapier, IFTTT, Shortcuts and a personal Slack workspace</li>
 						<li>I use an iPhone 8+ as my mobile device for security and usability reasons - don't @ me</li>
 						<li>I have a gaming desktop, which I seldom use and is mostly used for cord cutting services (sonarr, radarr etc)</li>
-						<li>You can find the setup for my machines (which are standardized) [here](https://github.com/joshghent/dotfiles)</li>
+						<li>You can find the setup for my machines (which are standardized) <a href="https://github.com/joshghent/dotfiles">here</a></li>
 					</ul>
 				</div>
 			</Layout>
