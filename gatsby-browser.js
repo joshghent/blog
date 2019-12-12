732d5cc38e36e011ebddfc659bfa72f840a469c4
Josh Ghent <josh.ghent@capp.co>
2019-12-12 16:15:36 +0000

    Updated version, information in gatsby config and corrected eslint issues

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-import "./src/styles/global.css"
+import 'typeface-montserrat';
+import 'typeface-merriweather';
+import './src/styles/global.css';
