991a3220ba9ad0821afb9a65f6fa3a0b9e2b6ff0
Josh Ghent <josh.ghent@capp.co>
2019-12-12 16:13:01 +0000

    Corrected all eslint issues

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
-function SEO({ description, lang, meta, keywords, title }) {
+function SEO({
+  description, lang, meta, keywords, title,
+}) {
   const { site } = useStaticQuery(
     graphql`
                   query {
@@ -22,10 +23,10 @@ function SEO({ description, lang, meta, keywords, title }) {
                       }
                     }
                   }
-                `
-  )
+                `,
+  );
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
@@ -45,63 +46,55 @@ function SEO({ description, lang, meta, keywords, title }) {
       defaultTitle={`${site.siteMetadata.title}`}
       meta={[
         {
-          name: `description`,
+          name: 'description',
           content: metaDescription,
         },
         {
-          property: `og:title`,
+          property: 'og:title',
           content: title,
         },
         {
-          property: `og:description`,
+          property: 'og:description',
           content: metaDescription,
         },
         {
-          property: `og:type`,
-          content: `website`,
+          property: 'og:type',
+          content: 'website',
         },
         {
-          name: `twitter:card`,
-          content: `summary`,
+          name: 'twitter:card',
+          content: 'summary',
         },
         {
-          name: `twitter:creator`,
+          name: 'twitter:creator',
           content: site.siteMetadata.author,
         },
         {
-          name: `twitter:title`,
+          name: 'twitter:title',
           content: title,
         },
         {
-          name: `twitter:description`,
+          name: 'twitter:description',
           content: metaDescription,
         },
       ]
         .concat(
           keywords.length > 0
             ? {
-              name: `keywords`,
-              content: keywords.join(`, `),
+              name: 'keywords',
+              content: keywords.join(', '),
             }
-            : []
+            : [],
         )
         .concat(meta)}
     />
-  )
+  );
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: 'en',
   meta: [],
   keywords: [],
-}
+};
 
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.array,
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string.isRequired,
-}
-
-export default SEO
+export default SEO;
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,38 +1,36 @@
-import "../styles/global.css";
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import '../styles/global.css';
+import Typography from 'typography';
+import Wordpress2016 from 'typography-theme-wordpress-2016';
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-    a: {
-      color: 'var(--textLink)',
-    },
-    // gatsby-remark-autolink-headers - don't underline when hidden
-    'a.anchor': {
-      boxShadow: 'none',
-    },
-    // gatsby-remark-autolink-headers - use theme colours for the link icon
-    'a.anchor svg[aria-hidden="true"]': {
-      stroke: 'var(--textLink)',
-    },
-    hr: {
-      background: 'var(--hr)',
-    },
-  }
-}
+Wordpress2016.overrideThemeStyles = () => ({
+  'a.gatsby-resp-image-link': {
+    boxShadow: 'none',
+  },
+  a: {
+    color: 'var(--textLink)',
+  },
+  // gatsby-remark-autolink-headers - don't underline when hidden
+  'a.anchor': {
+    boxShadow: 'none',
+  },
+  // gatsby-remark-autolink-headers - use theme colours for the link icon
+  'a.anchor svg[aria-hidden="true"]': {
+    stroke: 'var(--textLink)',
+  },
+  hr: {
+    background: 'var(--hr)',
+  },
+});
 
-delete Wordpress2016.googleFonts
+delete Wordpress2016.googleFonts;
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Wordpress2016);
 
 // Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles();
 }
 
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export default typography;
+export const { rhythm } = typography;
+export const { scale } = typography;
