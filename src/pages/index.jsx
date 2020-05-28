d47eae2de8536f68b9a88a3f6413408ee4fc2586
Josh Ghent <me@joshghent.com>
2020-05-28 08:45:27 +0100

    Updated styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ class HomeIndex extends React.Component {
       <Layout location={location} title={siteTitle}>
         <SEO
           title="Josh Ghent"
-          keywords={['blog', 'gatsby', 'javascript', 'react', 'josh', 'ghent', 'josh ghent', 'leicesterjs']}
+          keywords={['blog', 'gatsby', 'javascript', 'react', 'josh', 'ghent', 'josh ghent', 'leicesterjs', 'turboapi', 'aws consultancy', 'software resiliency', 'software consultancy']}
         />
         <main className="homepage">
           <section className="header" style={{ padding: '4vw 2vw' }}>
@@ -26,36 +26,38 @@ class HomeIndex extends React.Component {
           <section className="triangle" />
           <section className="cta" style={{ padding: `${rhythm(1.5)} ${rhythm(3)}` }}>
             <div className="row">
-              <div className="left">
+              <div className="left questions">
                 <p>AWS Bills making you sweat?</p>
                 <p>Developers fearing their on-call shift?</p>
                 <p>Mind-numbing manual work when things go wrong?</p>
               </div>
 
               <div className="right mailbox">
-                <p>I can help cut your cloud costs and save your developers time</p>
+                <p className="bold">I can help cut your cloud costs and save your developers time</p>
                 <p>Share your stories of despair <span role="img" aria-label="mail-icon">💌</span> <a className="email-address" href="mailto:me@joshghent.com?subject=Why does nothing work?">me@joshghent.com</a></p>
               </div>
             </div>
           </section>
           <section className="services">
-            <div className="bills">
-              <h1>Lowering your AWS Bills</h1>
-              <p>No one likes big bills, least of all when you're not sure where they came from. I can help by diving into your infrastructure and producing a comprehensive and actionable report to help you lower you bill by 20% or more.</p>
-              <p>You might feel like your AWS Bill is like a black hole, not sure what's in there and will probably hurt you if you go near it - with my in-depth analysis I will guarantee to save you far more than AWS rightsizing recommendations or any other infrastructure analysis tool.</p>
-              <p>You can get the peace of mind of knowing what your paying for and why you're paying for it. Most importantly, we help you scale your business whilst flat-lining your bill.</p>
-            </div>
-            <div className="resiliency">
-              <h1>Make your platform rock solid</h1>
-              <p>Early on, resiliency and scaling are an after thought for us all. But as your business grows, people complain, developers tear their hair out and everyone is left unhappy.</p>
-              <p>I can help you make your new or existing platform more resilient with my years of experience building systems integrated with hundreds of third parties.</p>
-              <p>I will mentor your developers to become resiliency rockstars so you can build and maintain a solid platform.</p>
-            </div>
-            <div className="automation">
-              <h1>Automation - less Terminator, more Wall-E</h1>
-              <p>Ever feel like you've done this same task over and over again? Or do hear your developers complain how difficult releasing new builds is? Or how long it takes?</p>
-              <p>I can provide the expertise to automate your software testing and delivery pipelines. Saving thousands of hours of manual QA time whilst increasing developer productivity.</p>
-              <p>Software testing and continuous integration can often seem inaccessible and expensive, but with my guidance your business can ship quality product, faster.</p>
+            <div className="services-container">
+              <div className="bills item">
+                <h1>Lowering your AWS Bills</h1>
+                <p>No one likes big bills, least of all when you're not sure where they came from. I can help by diving into your infrastructure and producing a comprehensive and actionable report to help you lower you bill by 20% or more.</p>
+                <p>You might feel like your AWS Bill is like a black hole, not sure what's in there and will probably hurt you if you go near it - with my in-depth analysis I will guarantee to save you far more than AWS rightsizing recommendations or any other infrastructure analysis tool.</p>
+                <p>You can get the peace of mind of knowing what your paying for and why you're paying for it. Most importantly, we help you scale your business whilst flat-lining your bill.</p>
+              </div>
+              <div className="resiliency item">
+                <h1>Make your platform rock solid</h1>
+                <p>Early on, resiliency and scaling are an after thought for us all. But as your business grows, people complain, developers tear their hair out and everyone is left unhappy.</p>
+                <p>I can help you make your new or existing platform more resilient with my years of experience building systems integrated with hundreds of third parties.</p>
+                <p>I will mentor your developers to become resiliency rockstars so you can build and maintain a solid platform.</p>
+              </div>
+              <div className="automation item">
+                <h1>Automation - less Terminator, more Wall-E</h1>
+                <p>Ever feel like you've done this same task over and over again? Or do hear your developers complain how difficult releasing new builds is? Or how long it takes?</p>
+                <p>I can provide the expertise to automate your software testing and delivery pipelines. Saving thousands of hours of manual QA time whilst increasing developer productivity.</p>
+                <p>Software testing and continuous integration can often seem inaccessible and expensive, but with my guidance your business can ship quality product, faster.</p>
+              </div>
             </div>
           </section>
         </main>
