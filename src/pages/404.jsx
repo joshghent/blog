75a734c9cd5c48cb5ba990d1d724912c0a07b018
Josh Ghent <josh.ghent@capp.co>
2020-08-25 14:14:45 +0100

    Removed gif 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -13,7 +13,6 @@ class NotFoundPage extends React.Component {
       <Layout location={location} title={siteTitle}>
         <SEO title="404: Not Found" description="This page could not be found on joshghent.com" />
         <h1>Not Found</h1>
-        <div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/g01ZnwAUvutuK8GIQn" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/high-quality-highqualitygifs-g01ZnwAUvutuK8GIQn">via GIPHY</a></p>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </Layout>
     );
