7a95f6333cf8fe1ac52cf26d26e81a2ba74b3cc1
Josh Ghent <me@joshghent.com>
2021-03-31 11:51:22 +0100

    Added components for schema and sign up form

---
+++ src/components/schema.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+function SchemaOrg({
+  author,
+  canonicalUrl,
+  datePublished,
+  defaultTitle,
+  description,
+  image,
+  isBlogPost,
+  title,
+  url,
+}) {
+  const baseSchema = [
+    {
+      '@context': 'http://schema.org',
+      '@type': 'WebSite',
+      url,
+      name: title,
+      alternateName: defaultTitle,
+    },
+  ];
+
+  const schema = isBlogPost
+    ? [
+      ...baseSchema,
+      {
+        '@context': 'http://schema.org',
+        '@type': 'BlogPosting',
+        url,
+        name: title,
+        alternateName: defaultTitle,
+        headline: title,
+        image: {
+          '@type': 'ImageObject',
+          url: image,
+        },
+        description,
+        author: {
+          '@type': 'Person',
+          name: author.name,
+          email: author.email,
+        },
+        mainEntityOfPage: {
+          '@type': 'WebSite',
+          '@id': canonicalUrl,
+        },
+        datePublished,
+      },
+    ]
+    : baseSchema;
+
+  return (
+    <>
+      <script script type="application/ld+json">{JSON.stringify(schema)}</script>
+    </>
+  );
+}
+
+export default SchemaOrg;
---
+++ src/components/signup-form.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { graphql, StaticQuery } from 'gatsby';
+
+class SignupForm extends React.Component {
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+        query HeadingQuery {
+          site {
+            siteMetadata {
+              tinyletterUrl
+            }
+          }
+        }
+      `}
+        render={(data) => (
+          <form
+            action={`${data.site.siteMetadata.tinyletterUrl}`}
+            method="post"
+            target="popupwindow"
+            onSubmit={() => {
+              window.open(
+                `${data.site.siteMetadata.tinyletterUrl}`,
+                'popupwindow',
+                'scrollbars=yes,width=800,height=600',
+              );
+              return true;
+            }}
+            className="SignupForm"
+          >
+            <h2>Get notified of my new blog posts and links from around the web that help you become a better developer. Aimed at developers of all experience levels.</h2>
+            <div className="Wrapper">
+              <input
+                aria-label="Email address"
+                placeholder="Your email address will never be shared or sold"
+                name="email"
+                type="text"
+                required
+                id="tlemail"
+              />
+              <input type="hidden" value="1" name="embed" />
+              <button type="submit"><span role="img" aria-label="love-letter">💌</span> Subscribe via Email</button>
+            </div>
+          </form>
+        )}
+      />
+    );
+  }
+}
+export default SignupForm;
