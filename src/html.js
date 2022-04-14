fab1adb6e5cbdbd66074740512d96fa3923a02c3
Josh Ghent <me@joshghent.com>
2022-04-14 16:11:49 +0100

    Updated colorscheme for safari

--- src/html.js
+++ src/html.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React from "react";
 
 export default function HTML(props) {
   return (
@@ -14,11 +14,34 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta property="og:image" content="https://joshghent.com/images/icon.png" />
-        <meta property="twitter:image" content="https://joshghent.com/images/icon.png" />
+        <meta
+          property="og:image"
+          content="https://joshghent.com/images/icon.png"
+        />
+        <meta
+          property="twitter:image"
+          content="https://joshghent.com/images/icon.png"
+        />
 
-        <link rel="webmention" href="https://webmention.io/joshghent.com/webmention" />
-        <link rel="pingback" href="https://webmention.io/joshghent.com/xmlrpc" />
+        <meta
+          name="theme-color"
+          content="#fff"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#000"
+          media="(prefers-color-scheme: dark)"
+        />
+
+        <link
+          rel="webmention"
+          href="https://webmention.io/joshghent.com/webmention"
+        />
+        <link
+          rel="pingback"
+          href="https://webmention.io/joshghent.com/xmlrpc"
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
