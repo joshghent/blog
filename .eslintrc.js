e2d820f4eef44d0cf750870c061ca1df6d34fea8
Josh Ghent <me@joshghent.com>
2022-07-17 16:53:11 +0100

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['airbnb', 'plugin:jsx-a11y/recommended'],
+  extends: ["airbnb", "plugin:jsx-a11y/recommended", "prettier"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
     graphql: true,
     __PATH_PREFIX__: true,
     __BASE_PATH__: true,
@@ -15,33 +15,23 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 2020,
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    'graphql',
-    'jsx-a11y',
-  ],
+  plugins: ["react", "graphql", "jsx-a11y"],
   rules: {
-    'react/jsx-one-expression-per-line': ['off'],
-    'react/prefer-stateless-function': ['off'],
-    'react/prop-types': ['off'],
-    'max-len': ['off'],
-    'react/no-danger': ['off'],
-    'jsx-a11y/label-has-associated-control': [
+    "react/jsx-one-expression-per-line": ["off"],
+    "react/prefer-stateless-function": ["off"],
+    "react/prop-types": ["off"],
+    "max-len": ["off"],
+    "react/no-danger": ["off"],
+    "jsx-a11y/label-has-associated-control": [
       2,
       {
-        labelComponents: [
-          '',
-        ],
-        labelAttributes: [
-          '',
-        ],
-        controlComponents: [
-          'TextInput',
-        ],
-        assert: 'either',
+        labelComponents: [""],
+        labelAttributes: [""],
+        controlComponents: ["TextInput"],
+        assert: "either",
         depth: 5,
       },
     ],
