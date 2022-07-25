da06add76d2bae08392e3c2e76d403855c5e1b3e
Josh Ghent <me@joshghent.com>
2022-07-25 15:58:49 +0100

    Fixed build

---
+++ src/pages/__tests__/index.spec.js
@@ -0,0 +1,26 @@
+import "@testing-library/jest-dom";
+import puppeteer from "puppeteer";
+
+const { toMatchImageSnapshot } = require("jest-image-snapshot");
+
+expect.extend({ toMatchImageSnapshot });
+
+describe("Homepage", () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+  });
+
+  it("renders correct", async () => {
+    const page = await browser.newPage();
+    await page.goto("http://localhost:8000");
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
