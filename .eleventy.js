193408b05e4a99a2b062cf4c1ed8d0ac95784c9e
Josh Ghent <me@joshghent.com>
2023-12-18 22:33:19 +0000

    fixed date display on posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,31 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-
-module.exports = function(eleventyConfig) {
-
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPassthroughCopy("src/img");
 
-
-  const {
-    DateTime
-  } = require("luxon");
+  const { DateTime } = require("luxon");
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
-    });
-
-    eleventyConfig.addFilter("readableDate", dateObj => {
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat("dd-MM-yy");
+      zone: "utc",
+    }).toFormat("yy-MM-dd");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc",
+    }).toFormat("MMMM d, yyyy");
+  });
+
+  eleventyConfig.addFilter("fromISO", (isoDateStr) => {
+    return DateTime.fromISO(isoDateStr, {
+      zone: "utc",
+    }).toFormat("MMMM d, yyyy");
   });
 
   return {
-    dir: { input: 'src', output: '_site' }
+    dir: { input: "src", output: "_site" },
   };
 };
