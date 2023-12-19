33003b97ceb047474d3c235045f2b3881770b254
Josh Ghent <me@joshghent.com>
2023-12-19 10:28:15 +0000

    added rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPassthroughCopy("src/img");
 
   const { DateTime } = require("luxon");
